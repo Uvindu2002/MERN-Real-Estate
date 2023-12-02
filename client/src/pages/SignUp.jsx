d2c6297af6c256edcf14318e88bd6eb0608a5336
Uvindu2002 <wupu0327@gmail.com>
2023-12-02 15:08:10 +0530

    Complte sign in page fuctionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -46,7 +46,7 @@ export default function SignUp() {
       <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
         <input type='text' placeholder='username' className='border p-3 rounded-lg' id='username' onChange={handleChange} />
         <input type='text' placeholder='email' className='border p-3 rounded-lg' id='email' onChange={handleChange} />
-        <input type='text' placeholder='password' className='border p-3 rounded-lg' id='password' onChange={handleChange} />
+        <input type='password' placeholder='password' className='border p-3 rounded-lg' id='password' onChange={handleChange} />
 
         <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
           {
@@ -57,7 +57,7 @@ export default function SignUp() {
       <div className='flex gap-2 mt-5'>
         <p>Have an account?</p>
         <Link to='/sign-in'>
-          <span className='text-blue-700'>Sign Up</span>
+          <span className='text-blue-700'>Sign In</span>
         </Link>
       </div>
       {
